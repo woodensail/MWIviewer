d128f5d0638f4a7bc16d6417b2e40b3861b5003a
woodensail <woodensail@hotmail.com>
2025-03-13 10:54:57 +0800

    Updates

---
+++ .umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  mfsu: false,
+  routes: [
+    { path: "/", component: "index" },
+    { path: "/docs", component: "docs" },
+  ],
+  npmClient: 'pnpm',
+});
