a9ece38ea0cebf4fca7e434b7ca0d44be2887d2e
woodensail <woodensail@hotmail.com>
2025-03-13 10:57:56 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "umi";
+import {defineConfig} from "umi";
 
 export default defineConfig({
   mfsu: false,
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+    {path: "/", component: "index"},
+    {path: "/docs", component: "docs"},
   ],
   npmClient: 'pnpm',
+  publicPath: '/MWIviewer/',
+  history: {type: 'hash'}
 });
